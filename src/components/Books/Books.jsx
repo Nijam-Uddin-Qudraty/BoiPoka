75c8a4c540a8f989d7eca847c221ca9028e378ac
Nijam-Uddin-Qudraty <129588187+Nijam-Uddin-Qudraty@users.noreply.github.com>
2025-05-03 07:51:30 +0600

    feat: shown book data to home

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,13 +1,64 @@
-import React, { useContext } from 'react';
-import { BookDataContext } from '../Root.jsx';
+import React, { useContext } from "react";
+import { BookDataContext } from "../Root.jsx";
 const Books = () => {
-    const books = useContext(BookDataContext);
-    const { bookId,bookName,author,image,category,tags } = books;
-    return (
-        <div>
-            <h1>{books.length}</h1>
-        </div>
-    );
+	const books = useContext(BookDataContext);
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			{books.map((book) => (
+				<div className="card bg-base-100 w-96 shadow-sm">
+					<figure>
+						<img src={book.image} alt="book" />
+					</figure>
+					<div className="card-body">
+						{book.tags.map((tag) => (
+							<p className="badge badge-outline">{tag}</p>
+						))}
+						<h2 className="card-title">
+							{book.bookName}
+							<div className="badge badge-secondary">NEW</div>
+						</h2>
+						<p>By : {book.author}</p>
+						<div className="card-actions flex justify-between">
+							<div className="">{book.category}</div>
+							<div className="rating">
+								<input
+									type="radio"
+									name="rating-2"
+									className="mask mask-star-2 bg-orange-400"
+									aria-label="1 star"
+								/>
+								<input
+									type="radio"
+									name="rating-2"
+									className="mask mask-star-2 bg-orange-400"
+									aria-label="2 star"
+									defaultChecked
+								/>
+								<input
+									type="radio"
+									name="rating-2"
+									className="mask mask-star-2 bg-orange-400"
+									aria-label="3 star"
+								/>
+								<input
+									type="radio"
+									name="rating-2"
+									className="mask mask-star-2 bg-orange-400"
+									aria-label="4 star"
+								/>
+								<input
+									type="radio"
+									name="rating-2"
+									className="mask mask-star-2 bg-orange-400"
+									aria-label="5 star"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Books;
